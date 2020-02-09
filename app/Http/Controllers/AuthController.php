0cd28347d66c8cb04a1e5e7396bdec5e5dba8a1e
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2020-02-09 19:35:36 +0700

    modify

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,8 +21,33 @@ class AuthController extends Controller
 
     public function callback()
     {
-        $user = Socialite::driver('Twitter')->user();
-        dd($user);
+        $providerUser = Socialite::driver('Twitter')->user();
+
+        // 既に存在するユーザーかを確認
+        $socialUser = SocialUser::where('provider_user_id', $providerUser->id)->first();
+
+        if ($socialUser) {
+            // 既存のユーザーはログインしてトップページへ
+            Auth::login($socialUser->user, true);
+            return redirect('/');
+        }
+
+        // 新しいユーザーを作成
+        $user = new User();
+        $user->unique_id = $providerUser->nickname;
+        $user->name = $providerUser->name;
+        $user->avatar = $providerUser->user['profile_image_url_https'];
+
+        $socialUser = new SocialUser();
+        $socialUser->provider_user_id = $providerUser->id;
+
+        DB::transaction(function () use ($user, $socialUser) {
+            $user->save();
+            $user->socialUsers()->save($socialUser);
+        });
+
+        Auth::login($user, true);
+        return redirect('/');
 
     }
 }
