6f375158f6aa15d6056d972fb2a55c5c731afc94
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2020-05-09 21:05:06 +0900

    modify

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+        $url->forceScheme('https');
         Schema::defaultStringLength(191);
     }
 }
