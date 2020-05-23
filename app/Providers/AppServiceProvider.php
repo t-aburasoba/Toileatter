672d930bac7f6cef97b152fb8bfed0b2d48748fa
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2020-05-23 17:43:04 +0900

    ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,11 +23,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        if(config('app.force_ssl')) {
-            \URL::forceScheme('https');
-        }
+        $url->forceScheme('https');
         Schema::defaultStringLength(191);
     }
 }
